859a50a4fda20300887ede031f0a546b44106661
Facundo Lemo <facundolemo@gmail.com>
2024-05-30 21:45:24 -0300

    working cart and better styles

--- cart.js
+++ cart.js
@@ -1,50 +1,76 @@
 
 const listarCarrito = async (carrito) => {
-    console.log(carrito)
     let acumulador = 0
     let div_carrito = ""
     carrito.forEach((p) => {
-        // let pString = JSON.stringify(p).replace(/'/g,"\\'").replace(/"/g,'&quot;')
+        let pString = JSON.stringify(p).replace(/'/g, "\\'").replace(/"/g, '&quot;')
         acumulador += p.price
         div_carrito += `
             <div class="row border shadow-sm mx-1 mb-2">
                 <div class="col-4">
-                    <img class="m-1 mw-100 shadow" src="${p.image}" alt="soy la imagen" style="width: 250px; height:150px; "/>
+                    <img class="m-1 mw-100 shadow" src="${p.image}" alt="${p.title}" style="width: 250px; height:150px; "/>
                 </div>
                 <div class="col-8 mw-100">
                     <h4 class="card-title">${p.title}</h4>
                     <p class="card-text">$${p.price}</p>
-                    <div class="mx-auto btn btn-danger bottom">¡¡¡¡SACAMEEE!!!!</div>
+                    <div class="mx-auto btn btn-danger bottom" onclick="eliminarProducto(${pString})">¡¡¡¡SACAMEEE!!!!</div>
                 </div> 
-
 
             </div> 
             `
     })
-    let div_checkout = 
-    `<div >
-        <p>
+    let div_checkout =
+        `<div >
+        <h3 class="flex-grow">
             TOTAL: $${acumulador}
-        </p>
-        <div class="mx-auto btn btn-success bottom">¡¡¡¡COMPRAAAAAAAARRRRRRRRRRRRRRR!!!!</div>
-
+        </h3>
+        <div class="mx-auto mb-2 btn btn-success bottom" onclick="checkout()">¡¡¡¡COMPRAARRRR!!!!</div>
     </div>
     `
     document.getElementById("div_carrito").innerHTML = div_carrito;
     document.getElementById("div_checkout").innerHTML = div_checkout;
 }
 
+const eliminarProducto = (p) => {
+    let carritostorage = JSON.parse(localStorage.getItem('carrito'))
+    const filtrado = carritostorage.filter(pc => pc.id !== p.id )
+    localStorage.setItem('carrito', JSON.stringify(filtrado))
+    listarCarrito(filtrado)
+}
+
+const limpiarCarrito = () => {
+    const carroVacio = []
+    localStorage.setItem('carrito', JSON.stringify(carroVacio))
+    listarCarrito(carroVacio)
+}
+
+const checkout = () => {
+    let carritostorage = JSON.parse(localStorage.getItem('carrito'))
+    if(carritostorage.length > 0){
+        const div_checkout =document.getElementById("div_checkout")
+        limpiarCarrito()
+        div_checkout.innerHTML.includes("Gracias") ? "" :  div_checkout.innerHTML+="<p>¡¡ Gracias por su compra !! :) </p>" 
+    }else{
+        document.getElementById("div_checkout").innerHTML += `<p>¡Carrito vacío! Ve a home y agrega productos.</p>`;
+    }
+}
+
 
 const main = async () => {
-    let carritostorage = localStorage.getItem('carrito')
-    let carrito;
-    if (carritostorage) {
-        carrito = JSON.parse(carritostorage)
-    } else {
-        carrito = []
-        localStorage.setItem('carrito', JSON.stringify(carrito))
+    try {
+        let carritostorage = localStorage.getItem('carrito')
+        let carrito;
+        if (carritostorage) {
+            carrito = JSON.parse(carritostorage)
+        } else {
+            carrito = []
+            localStorage.setItem('carrito', JSON.stringify(carrito))
+        }
+        await listarCarrito(carrito)
+
+    } catch (e) {
+        console.error("error agarrado: ",e)
     }
-    await listarCarrito(carrito)
 
 }
 
--- main.js
+++ main.js
@@ -4,16 +4,15 @@ const obtenerCategorias = async () => {
         let json = await respuesta.json();
         let categorias_html = '<a class="dropdown-item" href="">Ver todo</a>'
         json.forEach((c) => {
-            if (!c.includes("'")){
+            if (!c.includes("'")) {
                 categorias_html += `
                 <a class="dropdown-item" onclick="obtenerProductosCategoria('${c}')">Ver ${c}</a>
-                ` 
+                `
             } else {
-                let param = c.replace("'","___")
-                console.log(param)
+                let param = c.replace("'", "___")
                 categorias_html += `
                 <a class="dropdown-item" onclick="obtenerProductosCategoria('${param}')">Ver ${c}</a>
-                ` 
+                `
             }
         })
         document.getElementById("dropdown_categorias").innerHTML = categorias_html;
@@ -28,16 +27,18 @@ const obtenerProductosHome = async () => {
         let json = await respuesta.json();
         let productos_html = ""
         json.forEach((p) => {
-            let pString = JSON.stringify(p).replace(/'/g,"\\'").replace(/"/g,'&quot;')
+            let pString = JSON.stringify(p).replace(/'/g, "\\'").replace(/"/g, '&quot;')
             productos_html += `
-            <div class="card d-flex align-center p-2 m-4 shadow" style="width: 300px;">
-                <img class="card-img-top m-1 shadow" src="" alt="${p.title}" style="width: 250px; height:150px; />
+            <div class="card d-flex align-center p-2 m-4 shadow" style="width: 400px;">
+                <div class="text-center">
+                    <img class="card-img-top m-2 shadow" src="${p.image}" alt="${p.title}" style="width: auto; height:175px;" />
+                </div>
                 <div class="card-body">
-                    <h4 class="card-title">${p.title}</h4>
+                    <h4 class="card-title text-truncate">${p.title}</h4>
                     <p class="card-text">$${p.price}</p>
-                    <div class="d-flex flex-column justify-content-end">
+                    <div id="msg${p.title}" class="d-flex flex-column justify-content-end">
                         <a class="btn btn-primary bottom mb-2"  onclick="verProducto(${pString})" >Ver producto</a>
-                        <div class="btn btn-danger bottom" onclick="agregarCarrito(${pString})" >¡¡¡COMPRAR AHORA!!!</div>
+                        <div class="btn mb-2 btn-danger bottom" onclick="agregarCarrito(${pString})" >¡¡¡COMPRAR AHORA!!!</div>
                     </div>
                 </div>                
             </div> 
@@ -50,22 +51,22 @@ const obtenerProductosHome = async () => {
 }
 
 const obtenerProductosCategoria = async (c) => {
-    try{
-        if(c.includes("___")){
-            c = c.replace("___","'")
+    try {
+        if (c.includes("___")) {
+            c = c.replace("___", "'")
         }
         let respuesta = await fetch(`https://fakestoreapi.com/products/category/${c}`)
         let json = await respuesta.json();
         let productos_html = ""
         json.forEach((p) => {
-            let pString = JSON.stringify(p).replace(/'/g,"\\'").replace(/"/g,'&quot;')
+            let pString = JSON.stringify(p).replace(/'/g, "\\'").replace(/"/g, '&quot;')
             productos_html += `
             <div class="card d-flex align-center p-2 m-4 shadow" style="width: 300px;">
-                <img class="card-img-top m-1 shadow" src="" alt="${p.title}" style="width: 150px; height:auto;" />
+                <img class="card-img-top m-1 shadow" src="${p.image}" alt="${p.title}"  style="width: auto; height:175px;" />
                 <div class="card-body">
                     <h4 class="card-title">${p.title}</h4>
                     <p class="card-text">$${p.price}</p>
-                    <div class="d-flex flex-column justify-content-end">
+                    <div id="msg${p.title}" class="d-flex flex-column justify-content-end">
                         <a class="btn btn-primary bottom mb-2"  onclick="verProducto(${pString})" >Ver producto</a>
                         <div class="btn btn-danger bottom" onclick="agregarCarrito(${pString})" >¡¡¡COMPRAR AHORA!!!</div>
                     </div>
@@ -76,51 +77,55 @@ const obtenerProductosCategoria = async (c) => {
         document.getElementById("productos_html").innerHTML = productos_html;
     } catch (e) {
         console.error("error: ", e)
-}
+    }
 }
 
 const verProducto = async (p) => {
-    let productos_html =`
+    let pString = JSON.stringify(p).replace(/'/g, "\\'").replace(/"/g, '&quot;')
+    let productos_html = `
     <div class="row container justify-content-around mt-5">
-    <a onclick="obtenerProductosHome()" class="cursor-pointer">VOLVER A HOME</a>
-    <div class="col-7 border text-center">
-        <img class="text-center my-5" src="" style="width: 50%; height: auto;" alt="${p.title}"/>
+        <a onclick="obtenerProductosHome()" class="cursor-pointer">VOLVER A HOME</a>
+        <div class="col-7 shadow border text-center">
+            <img class="text-center my-5" src="${p.image}" style="width: 50%; height: auto;" alt="${p.title}"/>
+        </div>
+        <div class="col-4 shadow  border text-center">
+            <h1>${p.title}</h1>
+            <div>${p.category}</div>
+            <hr>
+            <div>${p.description}</div>
+            <hr>
+            <h3>$${p.price}</h3>
+            <div  class="btn btn-danger bottom mb-3" onclick="agregarCarrito(${pString})" >¡¡¡COMPRAR AHORA!!!</div>
+            <p class="text-center" id="msg${p.title}"></p>
+        </div>
     </div>
-    <div class="col-4 border text-center">
-        <h1>${p.title}</h1>
-        <div>${p.category}</div>
-        <hr>
-        <div>${p.description}</div>
-        <hr>
-        <h3>$${p.price}</h3>
-        <div class="btn btn-danger bottom mb-3" onclick="agregarCarrito(pString)" >¡¡¡COMPRAR AHORA!!!</div>
-    </div>
-</div>
     `
     document.getElementById("productos_html").innerHTML = productos_html;
-    console.log(p.title)
-    
 }
 
-const agregarCarrito = async (p) =>{
+const agregarCarrito = async (p) => {
     let carrito = JSON.parse(localStorage.getItem('carrito'))
     carrito.push(p)
-    console.log(carrito)
-    localStorage.setItem('carrito',JSON.stringify(carrito))
+    const div_producto = document.getElementById("msg" + p.title)
+    div_producto.innerHTML.includes("carrito.") ? "" : div_producto.innerHTML += '<p class="text-center" >Producto agregado al carrito. :) </p>'
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
 
 const main = async () => {
-    let carritostorage = localStorage.getItem('carrito')
-    if( carritostorage ){
-        let carrito = JSON.parse(carritostorage)
-    } else {
-        let carrito = []
-        localStorage.setItem('carrito',JSON.stringify(carrito))
+    try{
+        let carritostorage = localStorage.getItem('carrito')
+        if (carritostorage) {
+            let carrito = JSON.parse(carritostorage)
+        } else {
+            let carrito = []
+            localStorage.setItem('carrito', JSON.stringify(carrito))
+        }
+        await obtenerCategorias()
+        await obtenerProductosHome()
+    }catch (e){
+        console.error("error agarrado: ",e)
     }
-    await obtenerCategorias()
-    console.log('categorías agregadas')
-    await obtenerProductosHome()
-    console.log("productos agregados")
 }
 
 main()
